ed9250b091a113aa1c66bf16ee02aaeac72be90d
Marco Antonio Maciel <marco@mmaciel.local>
2018-06-12 15:41:25 +0200

    update pom.xml and main java class to make this project a WAR project

--- src/main/java/com/myco/assignment/AssignmentApplication.java
+++ src/main/java/com/myco/assignment/AssignmentApplication.java
@@ -2,13 +2,20 @@ package com.myco.assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  *
  * @author Marco Maciel
  */
 @SpringBootApplication
-public class AssignmentApplication {
+public class AssignmentApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AssignmentApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(AssignmentApplication.class, args);
